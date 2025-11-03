120b7a0f8eef15dc3d237182e64c1f6272bbce67
qetevann <queenling240@gmail.com>
2025-11-03 22:23:48 +0400

    Create classwork.js

---
+++ day113/classwork/classwork.js
@@ -0,0 +1,21 @@
+
+
+
+let names = {
+    name1: 'namee1',
+    name2: 'namee2',
+    name3: 'namee3',
+
+}
+
+names.name4 = 'namee4'
+names.name5 = 'namee5'
+
+console.log(names)
+
+delete names.name1
+delete names.name2
+delete names.name3
+
+console.log(names)
+
