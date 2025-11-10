3eb6980c3e642d4501c813e24a0ff948dc69207d
qetevann <queenling240@gmail.com>
2025-11-10 15:47:22 +0400

    1

--- day114/classwork/classwork.js
+++ day114/classwork/classwork.js
@@ -1,2 +1,34 @@
 
 
+// 1)შექმენი ობიექტი შენს შესახებ,შენი დავალებაა რომ გამოიტანო კიები და ვალუები ცალ ცალკე for in ის გამოყენებით
+
+
+
+let me = {
+  name: 'qeta',
+  age: 17,
+  hobby: 'coding',
+  city: 'tbilisi'
+}
+
+for (let key in me) {
+  console.log("key:", key)
+  console.log("value:", me[key])
+}
+
+
+// 2)შექმენი ობიექტი ავტომობილის შესახებ ,გქონდეს year , model , milage , color , შენი დავალებაა რომ შექმნა სია ვალუებისა,შემდეგ გადაუარო ამ სიას და გმაოიტანო ცალ ცალკე თითოეული value გამოიყენე for of
+
+
+let car = {
+  year: 2023,
+  model: 'dodge',
+  milage: 45000,
+  color: 'white'
+}
+
+let values = Object.values(car)
+
+for (let value of values) {
+  console.log(value)
+}
