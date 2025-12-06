835c558f3531d5b51f4f11408148d71ad30bf128
qetevann <queenling240@gmail.com>
2025-12-06 15:30:21 +0400

    1

---
+++ day122/homework/homework1.js
@@ -0,0 +1,18 @@
+function Cars (brand, year){
+    car1 = {
+        brand: 'mustang',
+        year: 2022
+    }
+
+    car2 = {
+        brand: 'corvette-c8',
+        year: 2020
+    }
+
+    car3 = {
+        brand: 'demon-170',
+        year: 2023
+    }
+
+}
+
---
+++ day122/homework/homework2.js
@@ -0,0 +1,20 @@
+function Person(name, lastname, age){
+    person1 = {
+        name: 'name1',
+        lastname: 'lastname1',
+        age: '1'
+    }
+
+    person2 = {
+        name: 'name2',
+        lastname: 'lastname2',
+        age: 'age2'
+    }
+
+    person3 = {
+        name: 'name3',
+        lastname: 'lastname3',
+        age: 'age3'
+    }
+
+}
