356c13ccfeef6c5e8bf7465b8360ee00dbb39f50
qetevann <queenling240@gmail.com>
2025-12-15 21:27:13 +0400

    1

---
+++ day124/homework/homework.js
@@ -0,0 +1,21 @@
+
+
+// const btn1 = document.getElementById("btn")
+//     const hex1 = document.getElementById("hex")
+
+//     const c = "0123456789ABCDEF"
+
+//     function colors() {
+
+//             for (let i = 0; i < 6; i++) {
+//             
+//     }
+//     return color
+//     }
+
+//     button.addEventListener("click", function() {
+//     const color1 = colors()
+//     document.body.style.bgcolor = color1
+//     hex1.textContent = color1
+//     })
+
