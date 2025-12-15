129fc1404a45dc829e8420bd1b2e396fb57bdd92
qetevann <queenling240@gmail.com>
2025-12-15 17:47:24 +0400

    1

--- day123/homework/homework.js
+++ day123/homework/homework.js
@@ -8,24 +8,31 @@ function Person(name, age, job){
             }
 }
 
-const name1 = document.getElementById('inpname')
-const age1 = document.getElementById('inpage')
-const job1 = document.getElementById('inpjob')
-const button = document.getElementById('btn')
+// const name1 = document.getElementById('nameinput')
+// const age1 = document.getElementById('ageinput')
+// const job1 = document.getElementById('jobinput')
+// const button = document.getElementById('btn')
 const resdiv = document.getElementById('result')
+const form = document.getElementById('form1')
 
-button.addEventListener('click', 
-    function (){
-        const nameval = name1.value
-        const ageval = age1.value
-        const jobval = job1.value
+const body = document.body
+
+form.addEventListener('submit', 
+    function (e){
+        e.preventDefault()
+        const person1 = new Person(
+            e.target.name.value,
+            e.target.age.value,
+            e.target.job.value
+        )
+
 
 
             const wr = document.createElement('div')
             const p = document.createElement('p')
 
-    p.textContent = user.info1()
+    p.textContent = person1.info1()
 
     wr.appendChild(p)
-    resdiv.appendChild(wr)
+    body.appendChild(wr)
 })
